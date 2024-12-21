2e9587fcad7162e03a2a159be15878e4c1e988a1
Macluster <deepakjudejohn@gmail.com>
2024-12-21 16:32:09 +0530

    Added conenction with postgress

---
+++ Risk_Management_RiskEX_Backend/Data/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Risk_Management_RiskEX_Backend.Data
+{
+    public class ApplicationDBContext:IdentityDbContext
+    {
+
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+
+        }
+    }
+}
--- Risk_Management_RiskEX_Backend/Program.cs
+++ Risk_Management_RiskEX_Backend/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Risk_Management_RiskEX_Backend.Data;
+DotNetEnv.Env.Load();
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING");
+builder.Services.AddDbContext<ApplicationDBContext>(options =>
+           options.UseNpgsql(connectionString));
+
 
 var app = builder.Build();
 
