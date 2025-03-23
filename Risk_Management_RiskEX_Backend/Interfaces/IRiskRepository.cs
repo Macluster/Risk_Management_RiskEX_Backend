2afffa64f36b63b7dcc3bfa9843171ad231bb849
Deepak Denny <deepak.denny@experionglobal.com>
2025-03-23 20:55:30 +0530

    CHANGES

--- Risk_Management_RiskEX_Backend/Interfaces/IRiskRepository.cs
+++ Risk_Management_RiskEX_Backend/Interfaces/IRiskRepository.cs
@@ -37,7 +37,7 @@ namespace Risk_Management_RiskEX_Backend.Interfaces
         Task<ICollection<RiskCategoryCountDTO>> GetRiskCategoryCounts(int?id);
         Task<object> GetAllRiskAssigned();
 
-        Task<string> SetAndGetRiskIdAsync(int? departmentId, int? projectId);
+        Task<string> SetAndGetRiskIdAsync(int ?departmentId, int? projectId);
 
 
 
